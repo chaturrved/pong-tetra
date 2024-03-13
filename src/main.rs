34c157e94f4ba75b05afb59d76ee48c20cd127a4
Chaturved D <chaturved.edu@gmail.com>
2024-03-13 23:27:47 +0530

    Initial builder or window state

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
 
-struct GameState {}
-
-impl State for GameState {}
-
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", 640, 480)
         .quit_on_escape(true)
         .build()?
         .run(|_| Ok(GameState {}))
+}
+
+struct GameState {}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        Ok(())
+    }
 }
